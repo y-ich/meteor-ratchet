573501530566bba2c1f3ecf2bc06587700e69f74
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2015-05-06 01:50:13 +0900

    polyfill

--- event-polyfill.js
+++ event-polyfill.js
@@ -23,3 +23,33 @@ try {
   CustomEvent.prototype = window.Event.prototype;
   window.CustomEvent = CustomEvent; // expose definition to window
 }
+
+// Polyfill for MouseEvent on IE9/10/11
+
+if (typeof MouseEvent !== 'function') {
+    (function (){
+        var _MouseEvent = window.MouseEvent;
+        window.MouseEvent = function (type, dict){
+            dict = dict | {};
+            var event = document.createEvent('MouseEvents');
+            event.initMouseEvent(
+                    type,
+                    (typeof dict.bubbles == 'undefined') ? true : !!dict.bubbles,
+                    (typeof dict.cancelable == 'undefined') ? false : !!dict.cancelable,
+                    dict.view || window,
+                    dict.detail | 0,
+                    dict.screenX | 0,
+                    dict.screenY | 0,
+                    dict.clientX | 0,
+                    dict.clientY | 0,
+                    !!dict.ctrlKey,
+                    !!dict.altKey,
+                    !!dict.shiftKey,
+                    !!dict.metaKey,
+                    dict.button | 0,
+                    dict.relatedTarget || null
+            );
+            return event;
+        }
+    })();
+}
